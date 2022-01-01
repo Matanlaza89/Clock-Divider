e89f6e0065075678194e16158810c7628a21cde3
Matan Leizerovich <matanlaza@gmail.com>
2022-01-01 18:04:05 +0200

    Update clockDividerTB.vhd

--- clockDividerTB.vhd
+++ clockDividerTB.vhd
@@ -1,9 +1,9 @@
 -----------------------------------------------------------------------------
-----------------	This RTL Code written by Matan Leizerovich  ---------------
+----------------  This RTL Code written by Matan Leizerovich  ---------------
 -----------------------------------------------------------------------------
 
 -----------------------------------------------------------------------------
--------			         Clock Divider TestBench			   	          -------
+-------			         Clock Divider TestBench              -------
 -----------------------------------------------------------------------------
 
 
@@ -16,7 +16,7 @@ end entity clockDividerTB;
 architecture sim of clockDividerTB is
 	-- constants
 	constant c_FREQ : natural := 1_000_000; -- Desired frequency - 1 MHz
-	constant C_PERIOD : time := 20 ns; -- T = 1/f => T=1/50*10^6 => T = 20nsec
+	constant c_PERIOD : time := 20 ns; -- T = 1/f => T=1/50*10^6 => T = 20nsec
 	
 	-- Stimulus signals --
 	signal i_clk   : std_logic;
@@ -42,8 +42,8 @@ begin
 	-- Testbench process --
 	p_TB : process
 	begin
-		i_reset <= '0'; wait for C_PERIOD;
-		i_reset <= '1'; wait for C_PERIOD * 15; 
+		i_reset <= '0'; wait for c_PERIOD;
+		i_reset <= '1'; wait for c_PERIOD * 15; 
 		wait;
 	end process p_TB;
 	
@@ -51,8 +51,8 @@ begin
 	-- 50 MHz clock in duty cycle of 50% - 20 ns --
 	p_clock : process 
 	begin 
-		i_clk <= '0'; wait for C_PERIOD/2; -- 10 ns
-		i_clk <= '1'; wait for C_PERIOD/2; -- 10 ns
+		i_clk <= '0'; wait for c_PERIOD/2; -- 10 ns
+		i_clk <= '1'; wait for c_PERIOD/2; -- 10 ns
 	end process p_clock;
 
 end architecture sim;
